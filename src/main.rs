0ea0b2d10a44f13197ab5374ef7ad4b1af494d59
shogun <shoguncrypto93@gmail.com>
2025-03-27 23:45:00 +0700

    [ft] add runtime

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
 mod balances;
 mod system;
 
+#[derive(Debug)]
+pub struct Runtime {
+    system: system::Pallet,
+    balances: balances::Pallet,
+}
+
+impl Runtime {
+    // Create a new instance of the main Runtime, by creating a new instance of each pallet.
+    fn new() -> Self {
+        Self {
+            system: system::Pallet::new(),
+            balances: balances::Pallet::new(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Init balance
+    let mut runtime = Runtime::new();
+    runtime.balances.set_balance(&"alice".to_string(), 100);
+
+    // start emulating a block
+    runtime.system.inc_block_number();
+    assert_eq!(runtime.system.block_number(), 1);
+
+    // first transaction
+    runtime.system.inc_nonce(&"alice".to_string());
+    let _res = runtime.balances
+        .transfer("alice".to_string(), "bob".to_string(), 30)
+        .map_err(|err| eprintln!("{:?}", err));
+
+    // second transaction
+    runtime.system.inc_nonce(&"alice".to_string());
+    let _res = runtime.balances
+        .transfer("alice".to_string(), "charlie".to_string(), 20)
+        .map_err(|err| eprintln!("{:?}", err));
+
+    println!("Runtime: {:#?}", runtime);
 }
--- src/system.rs
+++ src/system.rs
@@ -1,5 +1,6 @@
 use std::collections::BTreeMap;
 
+#[derive(Debug)]
 pub struct Pallet {
     block_number: u32,
     nonce: BTreeMap<String, u32>,
