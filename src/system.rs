a3f54b2c5d8bf71a17d5f6502d0cfad068dd8bac
shogun <shoguncrypto93@gmail.com>
2025-03-28 00:25:35 +0700

    [chor] make system pallet generic

--- src/system.rs
+++ src/system.rs
@@ -1,34 +1,44 @@
+use core::ops::AddAssign;
+use num::traits::{One, Zero};
 use std::collections::BTreeMap;
 
-type AccountId = String;
-type Nonce = u32;
-type BlockNumber = u32;
+
 
 #[derive(Debug)]
-pub struct Pallet {
+pub struct Pallet<AccountId, BlockNumber, Nonce> {
+
     block_number: BlockNumber,
+
     nonce: BTreeMap<AccountId, Nonce>,
 }
 
-impl Pallet {
+impl<AccountId, BlockNumber, Nonce> Pallet<AccountId, BlockNumber, Nonce>
+where
+    AccountId: Ord + Clone,
+    BlockNumber: Zero + One + AddAssign + Copy,
+    Nonce: Zero + One + Copy,
+{
+
     pub fn new() -> Self {
-        Self {
-            block_number: 0,
-            nonce: BTreeMap::new()
-        }
+        Self { block_number: BlockNumber::zero(), nonce: BTreeMap::new() }
     }
+
 
     pub fn block_number(&self) -> BlockNumber {
         self.block_number
     }
 
+
+
     pub fn inc_block_number(&mut self) {
-        self.block_number += 1
+        self.block_number += BlockNumber::one();
     }
 
+
+
     pub fn inc_nonce(&mut self, who: &AccountId) {
-        let nonce: Nonce = *self.nonce.get(who).unwrap_or(&0);
-        let new_nonce = nonce.checked_add(1).unwrap_or(nonce);
+        let nonce: Nonce = *self.nonce.get(who).unwrap_or(&Nonce::zero());
+        let new_nonce = nonce + Nonce::one();
         self.nonce.insert(who.clone(), new_nonce);
     }
 }
@@ -37,20 +47,12 @@ impl Pallet {
 mod test {
     #[test]
     fn init_system() {
-        /* TODO: Create a test which checks the following:
-            - Increment the current block number.
-            - Increment the nonce of `alice`.
-
-            - Check the block number is what we expect.
-            - Check the nonce of `alice` is what we expect.
-            - Check the nonce of `bob` is what we expect.
-        */
-        let mut system = super::Pallet::new();
+        let mut system = super::Pallet::<String, u32, u32>::new();
         system.inc_block_number();
         system.inc_nonce(&"alice".to_string());
 
         assert_eq!(system.block_number(), 1);
-        assert_eq!(system.nonce.get(&"alice".to_string()), Some(&1));
-        assert_eq!(system.nonce.get(&"bob".to_string()), None);
+        assert_eq!(system.nonce.get("alice"), Some(&1));
+        assert_eq!(system.nonce.get("bob"), None);
     }
 }
