86ec11464ec41a6e4fcaeb503aeaa01887f6bcf9
shogun <shoguncrypto93@gmail.com>
2025-03-28 00:43:06 +0700

    [ft] make system configurable

--- src/system.rs
+++ src/system.rs
@@ -2,52 +2,56 @@ use core::ops::AddAssign;
 use num::traits::{One, Zero};
 use std::collections::BTreeMap;
 
-
-
-#[derive(Debug)]
-pub struct Pallet<AccountId, BlockNumber, Nonce> {
-
-    block_number: BlockNumber,
-
-    nonce: BTreeMap<AccountId, Nonce>,
+pub trait Config {
+    type AccountId: Ord + Clone;
+    type BlockNumber: Zero + One + AddAssign + Copy;
+    type Nonce: Zero + One + Copy;
 }
 
-impl<AccountId, BlockNumber, Nonce> Pallet<AccountId, BlockNumber, Nonce>
-where
-    AccountId: Ord + Clone,
-    BlockNumber: Zero + One + AddAssign + Copy,
-    Nonce: Zero + One + Copy,
-{
+#[derive(Debug)]
+pub struct Pallet<T: Config> {
+    block_number: T::BlockNumber,
+    nonce: BTreeMap<T::AccountId, T::Nonce>,
+}
+
+impl<T: Config> Pallet<T> {
 
     pub fn new() -> Self {
-        Self { block_number: BlockNumber::zero(), nonce: BTreeMap::new() }
+        Self { block_number: T::BlockNumber::zero(), nonce: BTreeMap::new() }
     }
 
 
-    pub fn block_number(&self) -> BlockNumber {
+    pub fn block_number(&self) -> T::BlockNumber {
         self.block_number
     }
 
 
 
     pub fn inc_block_number(&mut self) {
-        self.block_number += BlockNumber::one();
+        self.block_number += T::BlockNumber::one();
     }
 
 
 
-    pub fn inc_nonce(&mut self, who: &AccountId) {
-        let nonce: Nonce = *self.nonce.get(who).unwrap_or(&Nonce::zero());
-        let new_nonce = nonce + Nonce::one();
+    pub fn inc_nonce(&mut self, who: &T::AccountId) {
+        let nonce: T::Nonce = *self.nonce.get(who).unwrap_or(&T::Nonce::zero());
+        let new_nonce = nonce + T::Nonce::one();
         self.nonce.insert(who.clone(), new_nonce);
     }
 }
 
 #[cfg(test)]
 mod test {
+    struct TestConfig;
+    impl super::Config for TestConfig {
+        type AccountId = String;
+        type BlockNumber = u32;
+        type Nonce = u32;
+    }
+
     #[test]
     fn init_system() {
-        let mut system = super::Pallet::<String, u32, u32>::new();
+        let mut system = super::Pallet::<TestConfig>::new();
         system.inc_block_number();
         system.inc_nonce(&"alice".to_string());
 
